f1efc83dc8fc5e47c1af2d800f3ac263b8cc73ae
seelnon <2bells1ravine@gmail.com>
2026-04-08 21:51:01 +0300

    Update events.js

--- Content/Projects/Endless_canvas/js/events.js
+++ Content/Projects/Endless_canvas/js/events.js
@@ -1,6 +1,6 @@
-import { state } from 'app/state.js';
-import { startStroke, addPointToStroke, endStroke, undo, redo, pickColor, deleteStrokeAt } from 'app/canvas.js';
-import { scheduleSave } from 'app/storage.js';
+import { state } from './state.js';
+import { startStroke, addPointToStroke, endStroke, undo, redo, pickColor, deleteStrokeAt } from './canvas.js';
+import { scheduleSave } from './storage.js';
 
 function getPointerPos(event) {
     return {
