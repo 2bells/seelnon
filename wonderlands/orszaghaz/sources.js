6926a2230b0b70b7fa2e44a77b4a711b2a3fce21
seelnon <2bells1ravine@gmail.com>
2026-04-11 18:48:41 +0300

    Create sources.js

---
+++ wonderlands/orszaghaz/sources.js
@@ -0,0 +1,22 @@
+export default {
+    version: "v0.0.1",
+    themeColor: "#ff0000",
+    avatarColor: "#000000",
+    avatar: "https://picsum.photos/seed/rick/100/100",
+    videos: {
+        "NEVER_GONNA_GIVE_YOU_UP": "dQw4w9WgXcQ"
+    },
+    images: {
+        "IMAGE_1": "https://i.pinimg.com/1200x/85/88/b5/8588b51da8972f104536b8902bcbe4b9.jpg", 
+        "IMAGE_2": "https://i.pinimg.com/736x/34/58/c1/3458c112a46b6ef95e86cd34c38c9ef9.jpg", 
+        "IMAGE_3": "https://i.pinimg.com/736x/39/a9/0d/39a90d16b00205f62ff3423d487312af.jpg"
+    },
+    labels: {
+        journal: "RICK_JOURNAL",
+        logs: "RICK_LOGS",
+        terminal: "[ RICK_TERMINAL ]",
+        description: "The Ultimate Prank",
+        wip: "NEVER GONNA GIVE YOU UP",
+        guid: "Rick GUID"
+    }
+};
