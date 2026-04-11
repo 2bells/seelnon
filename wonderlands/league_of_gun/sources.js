c2fc33d5d7ae518f0acd1e8d09fd5809eb46e17c
seelnon <2bells1ravine@gmail.com>
2026-04-11 18:23:51 +0300

    Create sources.js

---
+++ wonderlands/league_of_gun/sources.js
@@ -0,0 +1,22 @@
+export default {
+    version: "v1.0.1",
+    themeColor: "#4ecca3",
+    avatarColor: "#2d3436",
+    avatar: "https://picsum.photos/seed/miliastra/100/100",
+    videos: {
+        "TRAILER": "YERFXEsvH_k",
+        "PATCH_DEMO": "N75uN9ioY-s"
+    },
+    images: {
+        "CORE_REACTOR": "https://picsum.photos/seed/reactor/800/600",
+        "VOID_GATE": "https://picsum.photos/seed/void/800/600"
+    },
+    wipStatuses: ["wip", "forging", "stabilizing"],
+    labels: {
+        journal: "SECTOR_LOG",
+        logs: "ACCESS_TERMINAL",
+        terminal: "[ SECTOR_STABILIZATION_LOGS ]",
+        description: "Sector Data Archive",
+        wip: "STABILIZING SECTOR"
+    }
+};
