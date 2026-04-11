f4122f77831cbd80c50ee9f8fa43b598313b0ea5
seelnon <2bells1ravine@gmail.com>
2026-04-11 15:47:54 +0300

    Add files via upload

---
+++ wonderlands/miliastra_prime/sources.js
@@ -0,0 +1,21 @@
+export default {
+    version: "v1.0.1",
+    themeColor: "#4ecca3",
+    avatarColor: "#2d3436",
+    videos: {
+        "TRAILER": "YERFXEsvH_k",
+        "PATCH_DEMO": "N75uN9ioY-s"
+    },
+    images: {
+        "CORE_REACTOR": "https://picsum.photos/seed/reactor/800/600",
+        "VOID_GATE": "https://picsum.photos/seed/void/800/600"
+    },
+    wipStatuses: ["wip", "forging", "stabilizing"],
+    labels: {
+        journal: "SECTOR_LOG",
+        logs: "ACCESS_TERMINAL",
+        terminal: "[ SECTOR_STABILIZATION_LOGS ]",
+        description: "Sector Data Archive",
+        wip: "STABILIZING SECTOR"
+    }
+};
