792934cf184ba99daa8b10bfb120f0cfd3aacffd
seelnon <2bells1ravine@gmail.com>
2026-04-11 18:39:27 +0300

    Update sources.js

--- wonderlands/league_of_gun/sources.js
+++ wonderlands/league_of_gun/sources.js
@@ -1,17 +1,16 @@
 export default {
     version: "v1.0.1",
-    themeColor: "#4ecca3",
-    avatarColor: "#2d3436",
+    themeColor: "#00cc00",
+    avatarColor: "#000036",
     avatar: "https://picsum.photos/seed/miliastra/100/100",
     videos: {
-        "TRAILER": "-TQWngv-i24",
+        "LOG_TRAILER": "-TQWngv-i24",
         "PATCH_DEMO": "N75uN9ioY-s"
     },
     images: {
         "CORE_REACTOR": "https://picsum.photos/seed/reactor/800/600",
         "VOID_GATE": "https://picsum.photos/seed/void/800/600"
     },
-    wipStatuses: ["wip", "forging", "stabilizing"],
     labels: {
         journal: "SECTOR_LOG",
         logs: "ACCESS_TERMINAL",
