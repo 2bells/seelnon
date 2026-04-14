72718292323322a88d35e56d875a3229a05c1bb7
seelnon <2bells1ravine@gmail.com>
2026-04-14 20:50:36 +0300

    Update sources.js

--- wonderlands/orszaghaz/sources.js
+++ wonderlands/orszaghaz/sources.js
@@ -4,12 +4,13 @@ export default {
     avatarColor: "#000000",
     avatar: "https://picsum.photos/seed/rick/100/100",
     videos: {
-        "NEVER_GONNA_GIVE_YOU_UP": "dQw4w9WgXcQ"
+        "JAZZ": "FVIv7TaKdfU"
     },
     images: {
         "IMAGE_1": "https://i.pinimg.com/1200x/85/88/b5/8588b51da8972f104536b8902bcbe4b9.jpg", 
         "IMAGE_2": "https://i.pinimg.com/736x/34/58/c1/3458c112a46b6ef95e86cd34c38c9ef9.jpg", 
-        "IMAGE_3": "https://i.pinimg.com/736x/39/a9/0d/39a90d16b00205f62ff3423d487312af.jpg"
+        "IMAGE_3": "https://i.pinimg.com/736x/39/a9/0d/39a90d16b00205f62ff3423d487312af.jpg",
+        "Floor_Plan": "https://i.pinimg.com/1200x/25/06/f5/2506f5a2631bfae753b70edc6cd830d7.jpg"
     },
     labels: {
         journal: "RICK_JOURNAL",
