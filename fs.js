dcd4e70e59111aecf7f182b838a06d2c1cc95a58
seelnon <2bells1ravine@gmail.com>
2026-04-14 13:56:38 +0300

    Update fs.js

--- fs.js
+++ fs.js
@@ -94,6 +94,8 @@ export const FS = (() => {
   html('Rubber Animation', 'Content/Projects/Rubber_Animation/index.html', '/Projects', { icon: 'Content/Projects/Atelier/Atelier_thumbnail.jpg', description: 'A way to animate fluid pictures.' });  
   html('Video Analysis', 'Content/Projects/Video_Analysis/index.html', '/Projects', { icon: 'Content/Projects/Atelier/Atelier_thumbnail.jpg', description: 'A way to analyze a video.' });    
   html('Fireplace', 'Content/Projects/Fireplace/index.html', '/Projects', { icon: 'Content/Projects/Fireplace/Fireplace_Thumbnail.jpg', description: 'A way to remember an hour.' });  
+  html('ASCII animations', 'Content/Projects/ASCII/index.html', '/Projects', { icon: 'Content/Projects/Fireplace/Fireplace_Thumbnail.jpg', description: 'A way to vibe out an hour.' });  
+
 
 
   // Wonderlands
